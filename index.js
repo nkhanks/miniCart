c338cb5c548e0330ce9a444f3ee31f6bf8e1a9d4
nkhanks <n.mulelu@gmail.com>
2021-10-07 14:45:52 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 
-var access = require('./lib/accessControl.js')
-var cart = require('./lib/cart.js')
+const access = require('./lib/accessControl.js')
+const cart = require('./lib/cart.js')
 
-
-
-var fs = require('fs');
-const {v4: uuidv4} = require('uuid');
+const fs = require('fs');
 const express = require('express')
+
 
 const app = express()
 const port = 3000
@@ -16,88 +14,58 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
-app.post('/add-client', function (req, res)  {
-    var accessData = JSON.parse(fs.readFileSync('./data/access.json', 'utf8'));
+//Authorization Section
+app.post('/add-client', function (req, res) {
 
-    var isEntity = false;
-    var email = req.body.email;
+    let isAdded = access.addClient(req.body.email)
 
-    accessData.forEach((entity) => {
-        if (entity.email == email) {
-            isEntity = true
-        }
-    })
-
-    if (!isEntity) {
-        accessData.push({
-            "apiKey": "",
-            "email": email,
-            "permission": access.getPermission(email)
-        });
-    }
-
-    fs.writeFileSync("./data/access.json", JSON.stringify(accessData));
-
-    res.status(200).json({isAdded: true});
-})
-
-app.post('/login', function (req, res)  {
-    var accessData = JSON.parse(fs.readFileSync('./data/access.json', 'utf8'));
-
-    var isEntity = false;
-    var apiKey = uuidv4();
-    var email = req.body.email;
-
-    accessData.forEach((entity) => {
-        if (entity.email == email) {
-            isEntity = true
-            entity.apiKey = apiKey;
-        }
-    })
-
-    if (isEntity) {
-        fs.writeFileSync("./data/access.json", JSON.stringify(accessData));
-        res.status(200).json({token: apiKey});
+    if (isAdded) {
+        res.status(200).json({isAdded: isAdded});
     } else {
-        res.status(401).json({error: "you are not registered"});
+        res.status(200).json({isAdded: isAdded, error: "client already exists"});
+    }
+
+})
+
+app.post('/login', function (req, res) {
+    const token = access.hasAccess(req.body.email);
+
+    if (!token) {
+        res.status(401).json({error: "client does not exists"});
+    } else {
+        res.status(200).json({token: token});
     }
 
 })
 
 
 
-app.get('/test-auth', function (req, res)  {
-
-    
-    access.checkAccess(req, res, (apiKey ,req, res) => {
- res.send("Works!!!");
-    });
-
-   
-
-})
-
-app.get('/cart/get/:itemID', function (req, res)  {
-    
-    access.checkAccess(req, res, (apiKey ,req, res) => {
+app.get('/test-auth', function (req, res) {
 
 
-            cart.init(apiKey);
-            res.status(200).json(cart.get(req.params.itemID));
-
-
+    access.checkAccess(req, res, (data) => {
+        res.send("Works!!!");
     });
 
 })
 
-app.post('/cart/update/:itemID', function (req, res)  {
-    
-    access.checkAccess(req, res, (apiKey ,req, res) => {
+app.get('/cart/get/:itemID', function (req, res) {
+
+    access.checkAccess(req, res, (data) => {
+        cart.init(data);
+        res.status(200).json(cart.get(req.params.itemID));
+    });
+
+})
+
+app.post('/cart/update/:itemID', function (req, res) {
+
+    access.checkAccess(req, res, (data) => {
 
         if (("qty" in req.body)) {
-            cart.init(apiKey);
+            cart.init(data);
             res.status(200).json({
-                isUpdate: cart.update(req.params.itemID, req.body.qty),});
+                isUpdate: cart.update(req.params.itemID, req.body.qty), });
 
         } else {
 
@@ -108,10 +76,10 @@ app.post('/cart/update/:itemID', function (req, res)  {
 
 })
 
-app.get('/cart', function (req, res)  {
-    
-    access.checkAccess(req, res, (apiKey ,req, res) => {
-        cart.init(apiKey);
+app.get('/cart', function (req, res) {
+
+    access.checkAccess(req, res, (data) => {
+        cart.init(data);
         res.status(200).json(cart.getAll());
     });
 
@@ -119,11 +87,11 @@ app.get('/cart', function (req, res)  {
 
 })
 
-app.post('/cart/add/:itemID', function (req, res)  {
-    
-    access.checkAccess(req, res, (apiKey ,req, res) => {
+app.post('/cart/add/:itemID', function (req, res) {
+
+    access.checkAccess(req, res, (data) => {
         if (("qty" in req.body) && ("price" in req.body)) {
-            cart.init(apiKey);
+            cart.init(data);
             cart.add(req.params.itemID, req.body.price, req.body.qty)
             res.status(200).json({isAdded: true});
         } else {
@@ -134,10 +102,10 @@ app.post('/cart/add/:itemID', function (req, res)  {
 
 })
 
-app.delete('/cart/remove/:itemID', function (req, res)  {
-    
-    access.checkAccess(req, res, (apiKey ,req, res) => {
-        cart.init(apiKey);
+app.delete('/cart/remove/:itemID', function (req, res) {
+
+    access.checkAccess(req, res, (data) => {
+        cart.init(data);
         res.status(200).json({isRemoved: cart.remove(req.params.itemID)});
 
     });
@@ -145,33 +113,33 @@ app.delete('/cart/remove/:itemID', function (req, res)  {
 
 })
 
-app.post('/cart/empty', function (req, res)  {
-    
-    access.checkAccess(req, res, (apiKey ,req, res) => {
-        cart.init(apiKey);
+app.post('/cart/empty', function (req, res) {
+
+    access.checkAccess(req, res, (data) => {
+        cart.init(data);
         res.status(200).json({isRemoved: cart.empty()});
     });
 
 
 })
 
-app.delete('/cart/delete', function (req, res)  {
-    
-    access.checkAccess(req, res, (apiKey ,req, res) => {
-        res.status(200).json({isDeleted: cart.delete(apiKey)});
+app.delete('/cart/delete', function (req, res) {
+
+    access.checkAccess(req, res, (data) => {
+        res.status(200).json({isDeleted: cart.delete(data)});
     });
 
 })
 
 // 400: Bad Request
 app.use(function (req, res, next) {
-    res.json(400, {ERROR: 'Bad Request'});
+    res.status(400).json( {error: 'Bad Request'});
 });
 
 // 500: Error reporting
 app.use(function (err, req, res, next) {
     console.error(err.stack);
-    res.status(500).json({ERROR: 'Something Went Wrong Try Again Late'});
+    res.status(500).json({error: 'Something Went Wrong Try Again Late'});
 });
 
 app.listen(port, () => {
